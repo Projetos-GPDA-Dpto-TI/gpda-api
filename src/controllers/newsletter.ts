1a677171842db52714ae9cb3ee3d9107c8157d5a
Nicolas Greco <132113259+grecoww@users.noreply.github.com>
2024-03-10 01:55:24 +0000

    add `newsletter` controller and `news` model (signEmail) routine

--- src/controllers/newsletter.ts
+++ src/controllers/newsletter.ts
@@ -1,9 +1,25 @@
 import express, { Request, Response, Router } from 'express';
 import 'dotenv/config';
 
+import email from '../../infra/email';
+import news from '../models/news';
+
 const newsletterController: Router = express.Router();
 
 //cadastrar email
+newsletterController.post('/sign', (req, res) => {
+  const userEmail = req.body.email;
+
+  const signEmailResponse = news.signEmail(userEmail);
+
+  email.send({
+    from: { name: 'GPDA', address: 'naoresponda@gpda.com.br' },
+    to: userEmail,
+    subject: 'GPDA Newsletter',
+    text: 'Agora você está pronto para acompanhar a newsletter oficial da GPDA! Agora é só aguardar a próxima publicação.',
+  });
+  res.status(200).json(signEmailResponse);
+});
 
 //deletar email
 
--- src/models/news.ts
+++ src/models/news.ts
@@ -3,3 +3,13 @@ import database from '../../infra/database';
 //todo: function to validate unique email
 //todo: function to validate email form in client-side
 //todo: function to validate data structure before query
+
+async function signEmail(email: string): Promise<object> {
+  const response = await database.query({
+    text: 'INSERT INTO newsletter (email) VALUES $1 RETURNING email;',
+    values: [email],
+  });
+  return { email: response };
+}
+
+export default Object.freeze({ signEmail });
