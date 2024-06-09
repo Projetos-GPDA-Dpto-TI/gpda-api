85da3fed791dbe438c7393ee54ce155e1635e085
Nicolas Greco <132113259+grecoww@users.noreply.github.com>
2024-06-09 22:18:44 +0000

    centralize every setup routine in `yarn dev`

---
+++ infra/scripts/wait-for-postgres.js
@@ -0,0 +1,28 @@
+const { exec } = require('node:child_process');
+
+const loadingSpinner = ['⣾', '⣽', '⣻', '⢿', '⡿', '⣟', '⣯', '⣷'];
+let spinnerIndex = 0;
+
+function checkPostgres() {
+  exec('docker exec postgres-dev pg_isready --host localhost', handleReturn);
+
+  function handleReturn(error, stdout, stderr) {
+    if (stdout.search('accepting connections') === -1) {
+      showLoadingMessage();
+      setTimeout(checkPostgres, 100);
+      return;
+    }
+    console.log('\n\n🟢 Postgres está pronto e aceitando conexões\n');
+    return;
+  }
+}
+
+function showLoadingMessage() {
+  process.stdout.write(
+    `\r${loadingSpinner[spinnerIndex]} Aguardando o PostgreSQL aceitar a conexão...`
+  );
+  spinnerIndex = (spinnerIndex + 1) % loadingSpinner.length;
+}
+
+showLoadingMessage();
+checkPostgres();
