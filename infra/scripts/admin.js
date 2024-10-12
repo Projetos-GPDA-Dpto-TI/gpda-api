1760acba19f397fe275e28139942e52de57a77f7
Nicolas Greco <132113259+grecoww@users.noreply.github.com>
2024-10-12 02:17:19 +0000

    fix(build auth): add checkAdminExistance in `admin.js`

--- infra/scripts/admin.js
+++ infra/scripts/admin.js
@@ -2,6 +2,27 @@ import "dotenv/config";
 import "dotenv-expand/config";
 
 import user from "../../src/models/user.js";
+import database from "../database.js";
+
+async function setupAdmin() {
+  if (!(await checkAdminExistance())) {
+    createAdminUser();
+    console.log("Admin user created");
+  } else {
+    console.log("Admin user already created");
+  }
+}
+
+async function checkAdminExistance() {
+  const response = await database.query(
+    "SELECT * from member WHERE role='Admin';",
+  );
+  const parsedResponse = response.rowCount;
+  if (parsedResponse === 0) {
+    return false;
+  }
+  return true;
+}
 
 async function createAdminUser() {
   await user.signUser({
@@ -13,4 +34,4 @@ async function createAdminUser() {
   });
 }
 
-createAdminUser();
+setupAdmin();
